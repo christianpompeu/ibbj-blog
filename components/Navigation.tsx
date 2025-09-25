206885aec0f5cff746155f04986f8a186fcebf1c
Christian <christianpompeu@gmail.com>
2025-09-24 22:58:52 -0300

    Iniciado projeto

---
+++ components/Navigation.tsx
@@ -0,0 +1,3 @@
+export default function Navigation() {
+  return <nav className="sticky top-0 z-50 bg-background/80">Navigation</nav>;
+}
---
+++ lib/constants.ts
@@ -0,0 +1,6 @@
+export const navItems = [
+  { label: "Home", href: "/" },
+  { label: "Categories", href: "/categories" },
+  { label: "About", href: "/about" },
+  { label: "Contact", href: "/contact" },
+];
